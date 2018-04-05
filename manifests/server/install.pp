e7cc18604407fbaf4413c0f38285c31ae24370d7
Jianwen WEI <weijianwen@gmail.com>
2018-04-05 13:30:31 +0800

    Remove dependency for Yumrepo.

--- manifests/server/install.pp
+++ manifests/server/install.pp
@@ -1,25 +1,14 @@
 # Private class
 class x2go::server::install {
 
-  case $::osfamily {
-    'RedHat': {
-      $_package_require = Yumrepo['epel']
-    }
-    default: {
-      $_package_require = undef
-    }
-  }
-
   package { 'x2goserver':
     ensure  => 'present',
     name    => $x2go::server_package_name,
-    require => $_package_require,
   }
 
   package { 'x2goserver-xsession':
     ensure  => 'present',
     name    => $x2go::server_xsession_package_name,
-    require => $_package_require,
   }
 
 }
